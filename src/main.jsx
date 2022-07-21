3523e7128e0ba58f6d88b9e522075aeb982136fb
Xanul <rivas.060@gmail.com>
2022-07-20 20:08:58 -0500

    Added the useState hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HookApp } from './HookApp';
+import { CounterApp } from './01-useState/CounterApp';
+// import { HookApp } from './HookApp';
 import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HookApp />
+    <CounterApp />
   </React.StrictMode>
-)
+);
