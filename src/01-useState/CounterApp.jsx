de97069a828408a52863c1f9d0c7a09ff12a643d
Xanul <rivas.060@gmail.com>
2022-07-26 13:04:56 -0500

    Added the custom hoook

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -9,17 +9,15 @@ export const CounterApp = () => {
   });
 
   const {counter1, counter2, counter3} = state;
- 
   return (
     <>
-      <h1>Counter: {counter1}</h1>
-      <h1>Counter: {counter2}</h1>
-      <h1>Counter: {counter3}</h1>
+      <h1>Counter 1: {counter1}</h1>
+      <h1>Counter 2: {counter2}</h1>
+      <h1>Counter 3: {counter3}</h1>
       <hr />
-      {/* <button onClick={()=> setCounter(counter.counter1 - 1)}>-1</button> */}
-      <button onClick={()=> setCounter({
+      <button className="btn btn-success" onClick={() => setCounter({
         ...state,
-        counter1: counter1 +1
+        counter1: counter1 + 1,
       })}>+1</button>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './01-useState/CounterApp';
-// import { HookApp } from './HookApp';
-import './index.css';
+import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
