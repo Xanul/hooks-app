5b0fc170ca1c8494d8f3c975644819f8800c3037
Xanul <rivas.060@gmail.com>
2022-07-27 16:50:37 -0500

    Added more useEffect examples with Message component

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,70 +1,69 @@
 import { useEffect, useState } from "react"
+import { Message } from "./Message";
+
+
 
 export const SimpleForm = () => {
 
   const [formState, setFormState] = useState({
     username: 'Rodrigo',
-    email: 'rivas11@gmail.com',
-    age: 32
-  })
+    email: 'rivas11@gmail.com'
+  });
 
-  const {username, email, age} = formState;
+  const {username, email} = formState;
 
-  const onInputChange = ({target}) => {
+  const onInputChange = (eve) => {
+    const {target} = eve;
     const {name, value} = target;
-
-    // console.log(name, value);
 
     setFormState({
       ...formState,
-      [name]: value
-    })
+      [name]: value,
+    });
   }
 
-  useEffect(() => {
-    console.log('useEffect Called!');
-  }, []);
+  useEffect(()=> {
+    // console.log('useEffect called!');
+  },[]);
 
-  useEffect(() => {
-    console.log('formState Changed!');
-  }, [formState]);
+  useEffect(()=> {
+    // console.log('formState changed!');
+  },[formState]);
 
-  useEffect(() => {
-    console.log('email Changed!');
-  }, [email]);
+  useEffect(()=> {
+    // console.log('email changed!');
+    // console.log(email);
+  },[email]);
+
+
+  
 
   return (
     <>
-      <h1>Formulario Simple</h1>
+      <h1>Simple Form</h1>
       <hr />
 
       <input 
-        type="text" 
-        className="form-control"
-        placeholder="Username"
-        name="username"
-        value={username}
-        onChange={onInputChange}
+      type="text" 
+      className="form-control mt-3"
+      placeholder="Username"
+      name="username"
+      value={username}
+      onChange={onInputChange}
       />
 
       <input 
-        type="email" 
-        className="form-control mt-3"
-        placeholder="rivas@gmail.com"
-        name="email"
-        value={email}
-        onChange={onInputChange}
+      type="email" 
+      className="form-control mt-3"
+      placeholder="Email"
+      name="email"
+      value={email}
+      onChange={onInputChange}
       />
 
-      <input 
-        type="number" 
-        className="form-control mt-3"
-        placeholder="Age"
-        name="age"
-        value={age}
-        onChange={onInputChange}
-      />
-
+      {
+        (username === 'Rodrigo2') && <Message />
+      }
 
     </>
   )
