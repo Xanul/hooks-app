422dcc41be06f8a093f594c7d4dd5f00eef27490
Xanul <rivas.060@gmail.com>
2022-07-28 17:55:44 -0500

    Used two hooks in component

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,40 @@
-import { useFetch } from "../hooks/useFetch"
+import { useCounter, useFetch } from "../hooks/index.js";
+
+
 
 export const MultipleCustomHooks = () => {
+  const {counter, increment} = useCounter(1);
   
-  const {data, isLoading, hasError} = useFetch('https://www.breakingbadapi.com/api/quotes/1');
+  const { data, isLoading, hasError } = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+  // console.log({ data, isLoading, hasError });
   
-  console.log({data, isLoading, hasError});
+  const {author, quote} = !!data && data[0];
 
   return (
     <>
       <h1>BreakingBad Quotes</h1>
       <hr />
+        <h2>Counter Value = {counter}</h2>
+      <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-end">
+          <p className="mb-1">{quote}</p>
+          <footer className="blockquote-footer mt-1">{author}</footer>
+        </blockquote>
+      )}
+
+        <button 
+          className="btn btn-primary"
+          onClick={() => increment()}
+          disabled={isLoading}
+        >
+          Next Quote
+        </button>
+
     </>
-  )
-}
+  );
+};
--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,18 +1,18 @@
 import { useState } from "react"
 
 
-export const useCounter = (initialValue = 10) => {
+export const useCounter = (initialValue = 1) => {
   
   const [counter, setCounter] = useState(initialValue)
 
-  const increment = (value) => {
+  const increment = (value = 1) => {
     console.log(value)
-    setCounter(counter + value);
+    setCounter(counter + 1);
   }
 
   const decrement = (value = 1) => {
     // if (counter === 0) return;
-    setCounter(counter - value);
+    setCounter(counter - 1);
   }
 
   const reset = () => {
