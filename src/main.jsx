134461cfe37717efbbe834d78f8acbe3dd57bc88
Xanul <rivas.060@gmail.com>
2022-08-03 19:41:54 -0500

    Added the reducer intro example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Padre } from './07-tarea-memo/Padre';
+import './index.css';
+import './08-useReducer/intro-reducer'
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
@@ -15,8 +17,8 @@ import { Padre } from './07-tarea-memo/Padre';
 
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <Padre />
-  // </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//     <Padre />
+//   // </React.StrictMode>
+// );
