8e29968cd5099379f464b5c11005e7a88f1170ec
Xanul <rivas.060@gmail.com>
2022-08-05 13:29:55 -0500

    Finiched the TodoApp using custom hook with useReducer

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,44 +1,32 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
+import { useTodos } from "../hooks/useTodos";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
-import { todoReducer } from "./todoReducer";
-
 
 
 export const TodoApp = () => {
-  const initialState = [
-    {
-      id: new Date().getTime(),
-      description: "Recolectar la piedra del alma",
-      done: false,
-    },
-    {
-      id: new Date().getTime() * 3,
-      description: "Recolectar la piedra del tiempo",
-      done: false,
-    },
-  ];
 
-  const [todos, dispatch] = useReducer(todoReducer, initialState);
+  const {todos, todosCount, pendingTodosCount, handleNewTodo, handleDeleteTodo, handleToggleTodo} = useTodos();
 
-  const handleNewTodo = (todo) => {
-    console.log({todo});
-  }
-
+  
   return (
     <>
-      <h1>Todo App: 10 --- <small>Pendientes: 2</small></h1>
+      <h1>Todo App: {todosCount} --- <small>Pendientes: {pendingTodosCount}</small></h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos}/>
+          <TodoList 
+            todos={todos} 
+            onDeleteTodo={handleDeleteTodo}
+            onToggleTodo={handleToggleTodo}
+          />
         </div>
 
         <div className="col-5">
           <h4>Add Todo</h4>
           <hr />
-          <TodoAdd onNewTodo={handleNewTodo}/>
+          <TodoAdd onNewTodo={handleNewTodo} />
         </div>
       </div>
     </>
